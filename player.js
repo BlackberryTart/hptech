6f7ed1cc9f55a8ebe78863204725964592859b36
Kevin M <BlackberryTart@users.noreply.github.com>
2016-12-15 22:41:23 -0500

    Audioplayer now live on website

--- player.js
+++ player.js
@@ -88,6 +88,30 @@ function play() {
 	}
 }
 
+function HMS(seconds){
+	// multiply by 1000 because Date() requires miliseconds
+	var date = new Date(seconds * 1000);
+	var hh = date.getUTCHours();
+	var mm = date.getUTCMinutes();
+	var ss = date.getSeconds();
+	// If you were building a timestamp instead of a duration, you would uncomment the following line to get 12-hour (not 24) time
+	// if (hh > 12) {hh = hh % 12;}
+	// These lines ensure you have two-digits
+	if (hh < 10) {hh = "0"+hh;}
+	if (mm < 10) {mm = "0"+mm;}
+	if (ss < 10) {ss = "0"+ss;}
+	// This formats your string to HH:MM:SS
+	var t = hh+":"+mm+":"+ss;
+	return t;
+}
+
+// Time disp info
+function timeDisp(){
+	document.getElementById('time').innerHTML = HMS(duration);
+	document.getElementById('remain').innerHTML = HMS(music.currentTime);
+	setTimeout(timeDisp, 500);
+}
+
 // Gets audio file duration
 music.addEventListener("canplaythrough", function () {
 	duration = music.duration;  
